afe80d0160d38039225202f6b34e1b8f51fec294
Dominic Saadi <dominic.saadi@hey.com>
2021-11-04 00:44:17 -0700

    remove two serve tests

--- packages/cli/src/commands/__tests__/serve.test.js
+++ packages/cli/src/commands/__tests__/serve.test.js
@@ -40,59 +40,13 @@ jest.mock('@redwoodjs/api-server', () => {
 
 import yargs from 'yargs'
 
-import {
-  apiServerHandler,
-  bothServerHandler,
-  webServerHandler,
-} from '@redwoodjs/api-server'
+import { bothServerHandler } from '@redwoodjs/api-server'
 
 import { builder } from '../serve'
 
 describe('yarn rw serve', () => {
   afterEach(() => {
     jest.clearAllMocks()
-  })
-
-  it('Should proxy serve api with params to api-server handler', async () => {
-    const parser = yargs.command('serve [side]', false, builder)
-
-    parser.parse('serve api --port 5555 --apiRootPath funkyFunctions')
-
-    expect(apiServerHandler).toHaveBeenCalledWith(
-      expect.objectContaining({
-        port: 5555,
-        apiRootPath: expect.stringMatching(/^\/?funkyFunctions\/?$/),
-      })
-    )
-  })
-
-  it('Should proxy serve api with params to api-server handler (alias and slashes in path)', async () => {
-    const parser = yargs.command('serve [side]', false, builder)
-
-    parser.parse('serve api --port 5555 --rootPath funkyFunctions/nested/')
-
-    expect(apiServerHandler).toHaveBeenCalledWith(
-      expect.objectContaining({
-        port: 5555,
-        rootPath: expect.stringMatching(/^\/?funkyFunctions\/nested\/$/),
-      })
-    )
-  })
-
-  it('Should proxy serve web with params to web server handler', async () => {
-    const parser = yargs.command('serve [side]', false, builder)
-
-    parser.parse(
-      'serve web --port 9898 --socket abc --apiHost https://myapi.redwood/api'
-    )
-
-    expect(webServerHandler).toHaveBeenCalledWith(
-      expect.objectContaining({
-        port: 9898,
-        socket: 'abc',
-        apiHost: 'https://myapi.redwood/api',
-      })
-    )
   })
 
   it('Should proxy rw serve with params to appropriate handler', async () => {
